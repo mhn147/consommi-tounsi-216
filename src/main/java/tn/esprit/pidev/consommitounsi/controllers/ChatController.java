2371393cfb70fa34660e2944159808001364a007
khalil211 <khalilmajoul999@gmail.com>
2021-03-02 09:42:01 +0100

    chat url fix

--- src/main/java/tn/esprit/pidev/consommitounsi/controllers/ChatController.java
+++ src/main/java/tn/esprit/pidev/consommitounsi/controllers/ChatController.java
@@ -12,13 +12,13 @@ public class ChatController {
     @Autowired
     IChatService chatService;
 
-    @PostMapping("/Chats/{senderId}/{receiverId}")
+    @PostMapping("/chats/{senderId}/{receiverId}")
     @ResponseBody
     public void addChat(@RequestBody Chat c, @PathVariable("senderId")long senderId, @PathVariable("receiverId")long receiverId) {
         chatService.add(c, senderId, receiverId);
     }
 
-    @GetMapping("/Chats/{senderId}/{receiverId}")
+    @GetMapping("/chats/{senderId}/{receiverId}")
     @ResponseBody
     public List<Chat> getConversation(@PathVariable("senderId")long senderId, @PathVariable("receiverId")long receiverId) {
         return chatService.getConversation(senderId, receiverId);
